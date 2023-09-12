ee743c8b1fd8cb5ccfbc03479e508b792aa2f9c2
plastikSun <marcom2k01@gmail.com>
2023-09-12 18:35:18 +0200

    add: card

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <script>
 import Header from "./components/Header.vue";
+import Cards from "./components/Cards.vue";
+import store from "./store.js";
+
+import axios from "axios";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
   components: {
     Header,
+    Cards,
+  },
+
+  created() {
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+      .then((resp) => {
+        console.log(resp.data.data);
+        this.store.cardsArray = resp.data.data;
+      });
   },
 };
 </script>
 
 <template>
   <Header />
+  <main>
+    <div class="container mt-3">
+      <Cards />
+    </div>
+  </main>
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,48 @@
+<script>
+import store from "../store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <ul class="d-flex flex-wrap gap-4 justify-content-center">
+    <li v-for="(card, index) in store.cardsArray">
+      <div class="yugiho-card">
+        <img :src="card.card_images[0].image_url" alt="test" />
+        <div class="pt-3">
+          <h5>{{ card.name }}</h5>
+          <p class="mb-0" v-if="card.archetype">{{ card.archetype }}</p>
+          <p class="mb-0 fst-italic" v-else>No archetype available.</p>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+}
+
+.yugiho-card {
+  background-color: orange;
+  width: 200px;
+  min-height: 400px;
+  max-height: 450px;
+  text-align: center;
+
+  img {
+    max-width: 100%;
+  }
+
+  h5 {
+    font-size: 18px;
+  }
+}
+</style>
